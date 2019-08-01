9d8aa58bab6e5b7574e4c0df2c9ffad0c3d0e0ea
admin <421585364@qq.com>
2019-08-01 10:47:47 +0800

    s

--- src/com/sqldemo/CrateDFFByParquent.java
+++ src/com/sqldemo/CrateDFFByParquent.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SaveMode;
 
 /**
  *
@@ -16,9 +17,11 @@ public class CrateDFFByParquent {
         conf.setAppName("json").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
+
         DataFrame df = sqlContext.read().json("./jsonsql");
-
-
+        df.write().mode(SaveMode.Overwrite).parquet("./parquent");
+        DataFrame load = sqlContext.read().parquet("./parquent");
+        load.show();
 
     }
 }
